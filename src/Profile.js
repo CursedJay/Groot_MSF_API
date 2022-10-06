753ccc367be522026f50d1e1fdac115939a24188
JayG64 <jeremie.gamache@gmail.com>
2022-10-06 00:33:34 -0400

    get profile

---
+++ src/Profile.js
@@ -0,0 +1,19 @@
+function getProfile() {
+  const response = MSF.callApi('/player/v1/card');
+
+  if (!response) {
+    return false;
+  }
+
+  const responseCode = response.getResponseCode();
+
+  switch (responseCode) {
+    case 200:
+      const responseText = response.getContentText();
+      const result = JSON.parse(responseText);
+      return result?.data;
+    default:
+      console.error(`Error: code ${responseCode}`);
+      return false;
+  }
+}
