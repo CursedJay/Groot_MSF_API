0f9ed25e53d8ecd26399f0195ead588a4d6f545c
Wilson W <wilsonjwong@outlook.com>
2023-07-28 10:23:49 -0700

    [Changed] Inventory changes

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,13 +1,14 @@
-function getInventory(since = 'fresh') {
-  const INVENTORY_VERSION_CELL = getNamedRange('Inventory_Since');
-  const STATUS_CELL = getNamedRange('Inventory_Status');
+//TODO add param itemType
+function getGearInventory(since = 'fresh') {
+  //const INVENTORY_VERSION_CELL = getNamedRange('Inventory_Since');
+  //const STATUS_CELL = getNamedRange('Inventory_Status');
 
-  STATUS_CELL.setValue('🔴🔴🔴');
+  //STATUS_CELL.setValue('🔴🔴🔴');
 
   const response = MSF.callApi(`/player/v1/inventory?itemType=GEAR&since=${since}`);
 
   if (!response) {
-    STATUS_CELL.setValue('waiting for access');
+    //STATUS_CELL.setValue('waiting for access');
     return false;
   }
 
@@ -18,6 +19,25 @@ function getInventory(since = 'fresh') {
       const responseText = response.getContentText();
       const result = JSON.parse(responseText);
       const inventory = {};
+      inventory.gear = {};
+      inventory.since = result?.meta?.asOf ?? '';
+
+      //INVENTORY_VERSION_CELL.setValue(result?.meta?.asOf || '');
+      //STATUS_CELL.setValue('');
+
+      for (let i = 0; i < result?.data.length; i++) {
+        const { item, quantity } = result?.data[i];
+        inventory.gear[item] = quantity || 0;
+      }
+      return inventory;
+    case 344:
+      //STATUS_CELL.setValue('UNCHANGED');
+      return false;
+    default:
+      //STATUS_CELL.setValue(`Error: code ${responseCode}`);
+      return false;
+  }
+}
 
       INVENTORY_VERSION_CELL.setValue(result?.meta?.asOf || '');
       STATUS_CELL.setValue('');
@@ -28,10 +48,26 @@ function getInventory(since = 'fresh') {
       }
       return inventory;
     case 344:
-      STATUS_CELL.setValue('UNCHANGED');
+function getFullInventory(since = 'fresh') {
+  const response = MSF.callApi(`/player/v1/inventory?itemFormat=object&since=${since}`);
+
+  if (!response) {
+    //STATUS_CELL.setValue('waiting for access');
+    return false;
+  }
+
+  const responseCode = response.getResponseCode();
+
+  switch (responseCode) {
+    case 200:
+      const responseText = response.getContentText();
+      const result = JSON.parse(responseText);
+      return result;
+    case 344:
+      //STATUS_CELL.setValue('UNCHANGED');
       return false;
     default:
-      STATUS_CELL.setValue(`Error: code ${responseCode}`);
+      //STATUS_CELL.setValue(`Error: code ${responseCode}`);
       return false;
   }
 }
