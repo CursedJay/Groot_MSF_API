dfe4d8e386418579a3f57cdb816bd4108e3c83d9
JayG64 <jeremie.gamache@gmail.com>
2023-08-02 23:22:38 -0400

    [Added] Get playable characters from MSF API

--- src/Roster.js
+++ src/Roster.js
@@ -30,3 +30,26 @@ function getRoster(since = 'fresh') {
       return false;
   }
 }
+
+/** Get a list of the playable characters */
+function getCharacterList() {
+  const response = MSF.callApi(`/player/v1/characters?status=playable&charInfo=none`);
+
+  if (!response) {
+    return false;
+  }
+
+  const responseCode = response.getResponseCode();
+
+  switch (responseCode) {
+    case 200:
+      const responseText = response.getContentText();
+      const result = JSON.parse(responseText);
+
+      return result;
+    case 344:
+      return false;
+    default:
+      return false;
+  }
+}
