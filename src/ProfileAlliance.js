ae5f04d842c994d09ce4805b7c48c04441e287dc
CursedJay <jeremie.gamache@gmail.com>
2025-04-14 00:15:20 -0400

    Added Alliance Profile

---
+++ src/ProfileAlliance.js
@@ -0,0 +1,20 @@
+function getProfileAlliance() {
+  const response = callApi_('/player/v1/alliance/card');
+
+  if (!response) {
+    return false;
+  }
+
+  const responseCode = response.getResponseCode();
+
+  switch (responseCode) {
+    case 200: {
+      const responseText = response.getContentText();
+      const result = JSON.parse(responseText);
+      return result?.data;
+    }
+    default:
+      console.error(`Error: code ${responseCode}`);
+      return false;
+  }
+}
