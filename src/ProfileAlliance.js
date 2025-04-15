0b2d22b98e77af357c29afbdcac0f56b33b6c85a
CursedJay <jeremie.gamache@gmail.com>
2025-04-14 23:02:05 -0400

    [Added] Alliance Members

--- src/ProfileAlliance.js
+++ src/ProfileAlliance.js
@@ -1,12 +1,27 @@
-function getProfileAlliance() {
+function getAllianceProfile() {
   const response = callApi_('/player/v1/alliance/card');
 
-  if (!response) {
-    return false;
-  }
+  if (!response) return false;
 
   const responseCode = response.getResponseCode();
+  switch (responseCode) {
+    case 200: {
+      const responseText = response.getContentText();
+      const result = JSON.parse(responseText);
+      return result?.data;
+    }
+    default:
+      console.error(`Error: code ${responseCode}`);
+      return false;
+  }
+}
 
+function getAllianceMembers() {
+  const response = callApi_('/player/v1/alliance/members');
+
+  if (!response) return false;
+
+  const responseCode = response.getResponseCode();
   switch (responseCode) {
     case 200: {
       const responseText = response.getContentText();
